b0301a8481ca9bfa8604bc422442dac5aac32181
Euna Islam <euisla@ttu.ee>
2022-12-20 13:03:25 +0200

    Implemented product of array except self

--- ProductofArrayExceptSelf/ProductofArrayExceptSelf.java
+++ ProductofArrayExceptSelf/ProductofArrayExceptSelf.java
@@ -1,16 +1,21 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-       int product = 1;
-       int[] products = new int[nums.length];
-       products[0] = 1;
-       for(int i = 1; i < nums.length; i++) {
-          products[i] = products[i - 1] * nums[i - 1];
-       }
-       for(int i = products.length - 1; i > -1; i--) {
-          products[i] *= product;
-          product *= nums[i];
-       }
-       
-       return products;
+        int len = nums.length;
+        int[] result = new int[len];
+        int leftProduct = nums[0];
+        result[0] = 1;
+        int rightProduct = nums[len - 1];
+		
+        for(int i = 1; i < len; i++) {
+            result[i] = leftProduct;
+            leftProduct *= nums[i];
+        }
+
+        for(int i = len - 2; i >= 0; i--) {
+            result[i] *= rightProduct;
+            rightProduct *= nums[i];
+        }
+
+        return result;
     }
 }
