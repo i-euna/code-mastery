16b502f27953dd39119839942d02661173f08cb1
Euna Islam <euisla@ttu.ee>
2023-03-24 11:01:04 +0200

    Added solution for is xor string 2

---
+++ HackerRank/XORStrings2/README.md
@@ -0,0 +1,3 @@
+# Hackerank Problem: XOR Strings 2
+Problem Link: https://www.hackerrank.com/challenges/one-month-preparation-kit-strings-xor/problem<br/>
+How to use: use this code inside the Hackerank Result class
---
+++ HackerRank/XORStrings2/XORStrings2.java
@@ -0,0 +1,15 @@
+public class Solution {
+
+    public static String stringsXOR(String s, String t) {
+        String res = new String("");
+        for(int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == t.charAt(i))
+                res += "0";
+            else
+                res += "1";
+        }
+
+        return res;
+    }
+
+}
