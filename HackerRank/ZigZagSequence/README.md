7d50b423f3bc95b1fd99ebdda0346764beb642e8
Euna Islam <euisla@ttu.ee>
2023-03-25 08:05:27 +0200

    Added solution for is Zigzag sequence

---
+++ HackerRank/ZigZagSequence/README.md
@@ -0,0 +1,3 @@
+# Hackerank Problem: Zig Zag Sequence
+Problem Link: https://www.hackerrank.com/challenges/one-month-preparation-kit-zig-zag-sequence/problem<br/>
+How to use: use this code inside the Hackerank Result class
---
+++ HackerRank/ZigZagSequence/ZigZagSequence.java
@@ -0,0 +1,26 @@
+public class Solution {
+
+    public static void findZigZagSequence(int [] a, int n){
+        Arrays.sort(a);
+        int mid = (int)((n - 1) / 2);
+        int temp = a[mid];
+        a[mid] = a[n - 1];
+        a[n - 1] = temp;
+    
+        int st = mid + 1;
+        int ed = n - 2;
+        while(st <= ed){
+            temp = a[st];
+            a[st] = a[ed];
+            a[ed] = temp;
+            st = st + 1;
+            ed = ed - 1;
+        }
+        for(int i = 0; i < n; i++){
+            if(i > 0) System.out.print(" ");
+            System.out.print(a[i]);
+        }
+        System.out.println();
+    }
+
+}
