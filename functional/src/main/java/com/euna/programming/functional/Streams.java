435574528468c8c0c17f2f8fe4db5c77e7f3b480
Euna Islam <euna.islam@gmail.com>
2023-07-18 21:14:11 +0300

    Practiced streams

--- functional/src/main/java/com/euna/programming/functional/Streams.java
+++ functional/src/main/java/com/euna/programming/functional/Streams.java
@@ -14,6 +14,14 @@ public class Streams {
         String[] stringArray = {"Hello", "Bye", "Hi", "Wow", "Yes", "No"};
         List<String> listOfStrs = new ArrayList<>(Arrays.asList(stringArray));
 
+        Person[] personArray = {
+                new Person("Jin", 10, "student"),
+                new Person("Mugen", 15, "student"),
+                new Person("Momo", 20, "worker"),
+                new Person("So", 30, "worker"),
+        };
+        List<Person> listOfPersons = new ArrayList<>(Arrays.asList(personArray));
+
         //Functions
         Function<Integer, Integer> timesTwo = x -> x * 2;
         Predicate<Integer> isEven = x -> x % 2 == 0;
@@ -28,6 +36,11 @@ public class Streams {
                 .collect(Collectors.toList());
         System.out.println("Doubled integers " + doubled);
 
+        List<String> names = listOfPersons.stream()
+                .map(person -> person.name)
+                .collect(Collectors.toList());
+        System.out.println("Names: " + names);
+
         //Filter
         List<Integer> evens = listOfIntegers.stream()
                 .filter(isEven)
@@ -38,6 +51,11 @@ public class Streams {
                 .filter(isLongerThan3)
                 .collect(Collectors.toList());
         System.out.println("Long words " + longWords);
+
+        List<Person> olderThan15 = listOfPersons.stream()
+                .filter(person -> person.age > 15)
+                .collect(Collectors.toList());
+        System.out.println("Older than 15: " + olderThan15);
 
         //reduce
         System.out.println("Check accumulator");
@@ -50,6 +68,11 @@ public class Streams {
         Integer sum = listOfIntegers.stream()
                 .reduce(0, getSum);
         System.out.println("Sum with reduce " + sum);
+
+        Integer combinedAge = listOfPersons.stream()
+                        .map(person -> person.age)
+                        .reduce(0, getSum);
+        System.out.println("Older than 15: " + combinedAge);
 
         //collect
         Set<Integer> noDuplicates = listOfIntegers.stream()
@@ -79,5 +102,64 @@ public class Streams {
                 ));
         System.out.println("Word Partition: " + partitions);
 
+        //combine list
+        Integer totalStudentAge = listOfPersons.stream()
+                .filter(person -> person.profession == "student")
+                .map(student -> student.age)
+                .reduce(0, (acc, x) -> acc + x);
+
+        Long noOfStudent = listOfPersons.stream()
+                .filter(person -> person.profession == "student")
+                .collect(Collectors.counting());
+        float avgAgeOfStudents = totalStudentAge / noOfStudent;
+        System.out.println("Average student age: " + avgAgeOfStudents );
+        Integer totalWorkerAge = listOfPersons.stream()
+                .filter(person -> person.profession == "worker")
+                .map(student -> student.age)
+                .reduce(0, (acc, x) -> acc + x);
+
+        Long noOfWorker = listOfPersons.stream()
+                .filter(person -> person.profession == "worker")
+                .collect(Collectors.counting());
+        float avgAgeOfWorkers = totalWorkerAge / noOfWorker;
+        System.out.println("Average student age: " + avgAgeOfWorkers );
+
+        Map<String, Integer> averageAgeMap = listOfPersons.stream()
+                .collect(Collectors.groupingBy(
+                        person -> person.profession
+                ))
+                .entrySet()
+                .stream()
+                .collect(Collectors.toMap(
+                                (entry) -> entry.getKey(),
+                                (entry) -> entry.getValue()
+                                        .stream()
+                                        .map((person) -> person.age)
+                                        .reduce(0, (acc, x) -> acc + x) / entry.getValue().size())
+                );
+        System.out.println("Average age map: " + averageAgeMap );
+
+
+        //parallel streams - increases performance using multiple thread
+        List<String> processedWords = listOfStrs.parallelStream()
+                .map(str -> {
+                    System.out.println("To Uppercase " + str);
+                    return str.toUpperCase();
+                })
+                .collect(Collectors.toList());
+
+        System.out.println("Processed words: " + processedWords );
+    }
+
+    static class Person {
+        public String name;
+        public Integer age;
+
+        public String profession;
+        public Person(String name, Integer age, String profession) {
+            this.name = name;
+            this.age = age;
+            this.profession = profession;
+        }
     }
 }
